ff1c0cc5421be277c9ced56cc5b2b7a0c62dfc23
hyeji1221 <lhg2615@naver.com>
2020-08-20 02:00:01 +0900

    11404번 문제(Floyd Warshall)

---
+++ 백준알고리즘/11404.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX 100001
+using namespace std;
+int n, m; //n은 도시 수, m은 버스의 수
+int arr[100][100];
+int INF = 999999999;
+void floyd() {
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][k] + arr[k][j] < arr[i][j]) {
+					arr[i][j] = arr[i][k] + arr[k][j];
+				}
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i == j || arr[i][j] == INF) // 자기 자신에게 가는 경우는 0
+				cout << 0 << " ";
+			else
+				cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int start, finish, cost;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			arr[i][j] = INF; // 무한대로 초기화
+		}
+	}
+	for (int i = 0; i < m; i++) {
+		cin >> start >> finish >> cost;
+		//if (arr[start - 1][finish - 1] > cost)
+			arr[start - 1][finish - 1] = cost;
+	}
+	floyd();
+	return 0;
+}
